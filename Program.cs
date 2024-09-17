9893218796e67db6c8c20c63c2dbe21509f9390a
JayMayse <maysembcc@gmail.com>
2024-09-17 14:05:02 -0500

    random numbers

--- Program.cs
+++ Program.cs
@@ -12,83 +12,75 @@
             const int HOU3 = 346;
              
             Console.WriteLine("Let's get you some coins to play with!\nPlease enter a number from 1 to 5");
-            Console.ReadLine();
+            //Console.ReadLine();
 
             int[] minas = new int[4] { HOU7, HOU2, HOU8, HOU3 };
             Random rng = new Random();
             int randomMinas = rng.Next(0, 4);
             int GameCoins = minas[randomMinas];
 
-
             Console.WriteLine("You have " + GameCoins + " game coins to play with.\n\n");
-            //allot coins randomly
-
             Console.WriteLine("You can play center line for 1\nYou can play all 3 lines for 3\n");
-
-
             Console.WriteLine("*** How to WIn ***");
             Console.WriteLine("3x 713 wins $50\n3x 281 wins $35\n3x area codes# wins $30\n3x any other number wins $25");
             Console.WriteLine("713 + 2x any other wins $20\n2x area code# wins $15\narea code# +2x any other number wins 10\n2x any number wins $5");
 
-            // insert array set up with 3x3 vertical
-            //int[,] slots = new int [3,3];
-
-            int nulo = 0;
+            const int nulo = 0;
             int un = 1;
             int duex = 2;
+            int trois = 3;
+            int quad = 4;
+            int six = 6;
+            int sept = 7;
+            int huit = 8;
 
 
             if (GameCoins > 0)
             {
-
-                int[] bangers = new int[11] { 1, 2, 3, 4, 6, 7, 8, 832, 281, 713, 346 };
+                //int[] bangers = new int[11] { 1, 2, 3, 4, 6, 7, 8, 832, 281, 713, 346 };
                 Random rngspoke = new Random();
-                int clicks = rngspoke.Next(0, 10);
-                //int GameWheel = bangers[clicks];
+                int clicks1 = rngspoke.Next(0, 10);
+                int clicks2 = rngspoke.Next(0, 10);
+                int clicks3 = rngspoke.Next(0, 10);
+                //for (int i = n1; i <= n2; i++)
+                //{
+                //    for (int j = 0; j <= 10; j++)
+                //    {
+                //        num1[j] = rand.Next(n1, n2);
+                //    }
+                //}
 
-                //if (i = 0; 1 < x; i++)
-
-                //int[,] slots = new int[3, 3];
-                int[,] slots =    { {clicks, clicks, clicks}, 
-                                    {clicks, clicks, clicks}, 
-                                    {clicks, clicks, clicks},
-
+                int[,] slots =    { {clicks1, clicks2, clicks3},
+                                    {clicks1, clicks2, clicks3},
+                                    {clicks1, clicks2, clicks3},
                                    };
 
-                for (int i = 0; i < slots.GetLength(0); i++ )
+                for (int i = 0; i < slots.GetLength(0); i++)
                 {
                     for (int j = 0; j < slots.GetLength(1); j++)
                     {
-                        Console.Write(slots[i,j] + "");
+                        Console.Write(slots[i, j] + "");
                     }
                     Console.WriteLine();
                 };
-                //int randomnumber = 7;
-
-                //slots[0, 0] = randomnumber;
 
 
-                Console.WriteLine(slots[nulo, nulo]);
-                Console.WriteLine(slots[un, nulo]);
-                Console.WriteLine(slots[duex, nulo]);
-                Console.WriteLine("");
-                Console.WriteLine(slots[un, nulo]);
-                Console.WriteLine(slots[un, un]);
-                Console.WriteLine(slots[un, duex]);
-                Console.WriteLine("");
-                Console.WriteLine(slots[duex, nulo]);
-                Console.WriteLine(slots[duex, un]);
-                Console.WriteLine(slots[duex, duex]);
-
+                //Console.WriteLine(slots[nulo, nulo]);
+                //Console.WriteLine(slots[nulo, un]);
+                //Console.WriteLine(slots[nulo, duex]);
+                //Console.WriteLine("");
+                //Console.WriteLine(slots[un, nulo]);
+                //Console.WriteLine(slots[un, un]);
+                //Console.WriteLine(slots[un, duex]);
+                //Console.WriteLine("");
+                //Console.WriteLine(slots[duex, nulo]);
+                //Console.WriteLine(slots[duex, un]);
+                //Console.WriteLine(slots[duex, duex]);
             }
             //display coin count
-
             //ask how many coins to play
-
             //run slot
-
             //if statement on slot results
-
             //loop 
 
 
