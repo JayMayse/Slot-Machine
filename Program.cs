fa5ae136db8276a7ba24de218be79fc6ab6394f3
JayMayse <maysembcc@gmail.com>
2025-01-07 14:05:22 -0600

    Florian meeting update 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Slot_Machine
 {
@@ -12,6 +13,7 @@ namespace Slot_Machine
             const int HOU2 = 281;
             const int HOU8 = 832;
             const int HOU3 = 346;
+            //  int col = 0;
 
             Console.WriteLine("Let's get you some coins to play with!\nPlease enter a number from 1 to 5");
             Console.ReadLine();
@@ -23,30 +25,16 @@ namespace Slot_Machine
 
             Console.WriteLine("You have " + GameCoins + " game coins to play with.\n\n");
             Console.WriteLine("You can play center line for 1\nYou can play all 3 lines for 3\n");
-            Console.WriteLine("*** How to WIn ***");
-            Console.WriteLine("3x 713 wins $50\n3x 281 wins $35\n3x area codes# wins $30\n3x any other number wins $25");
-            Console.WriteLine("713 + 2x any other wins $20\n2x area code# wins $15\narea code# +2x any other number wins 10\n2x any number wins $5");
+            Console.WriteLine("*** How to Win ***");
+            //Console.WriteLine("3x 713 wins $50\n3x 281 wins $35\n3x area codes# wins $30\n3x any other number wins $25");
+            //Console.WriteLine("713 + 2x any other wins $20\n2x area code# wins $15\narea code# +2x any other number wins 10\n2x any number wins $5");
 
             if (GameCoins > 0)
             {
-                //int[] bangers = new int[11] { 1, 2, 3, 4, 6, 7, 8, 832, 281, 713, 346 };
                 Random rngspoke = new Random();
                 int clicks1 = rngspoke.Next(0, 10);
                 int clicks2 = rngspoke.Next(0, 10);
                 int clicks3 = rngspoke.Next(0, 10);
-                //for (int i = n1; i <= n2; i++)
-                //{
-                //    for (int j = 0; j <= 10; j++)
-                //    {
-                //        num1[j] = rand.Next(n1, n2);
-                //    }
-                //}
-
-                //int[,] slots =    { {clicks1, clicks2, clicks3},
-                //                    {clicks1, clicks2, clicks3},
-                //                    {clicks1, clicks2, clicks3},
-                //                   };
-
                 int[,] slots = new int[3, 3];
 
                 for (int row = 0; row < slots.GetLength(0); row++)
@@ -55,41 +43,30 @@ namespace Slot_Machine
                     {
                         slots[row, col] = rngspoke.Next(10);
                         Console.Write(slots[row, col]);
+
                     }
+
                     Console.WriteLine();
-                };
+                }
 
-                //if (GameCoins > 0)
-                //    for (int i = 0; i < GameCoins; i++)
-                //        //Console.WriteLine("How many coins do you want to play? ");
-                //        //int Playem = Convert.ToInt32(Console.ReadLine());
-                //        //Console.WriteLine("You will be playing " + Playem + " coins");
+                bool isWinner = true;
 
-                //        {
-                //            //int[] bangers = new int[11] { 1, 2, 3, 4, 6, 7, 8, 832, 281, 713, 346 };
-                //            Random rngspoke = new Random();
-                //            int clicks1 = rngspoke.Next(0, 10);
-                //            int clicks2 = rngspoke.Next(0, 10);
-                //            int clicks3 = rngspoke.Next(0, 10);
+                for (int col = 1; col < slots.GetLength(1); col++)
+                {
 
-                //            int[,] slots = new int[3, 3];
+                    if (slots[1, 0] != slots[1, col])
+                    { 
+                        isWinner = false;
+                      //Console.WriteLine("Looks like we got a winner!");
+                    }
 
-                //            for (int row = 0; row < slots.GetLength(0); row++)
-                //            {
-                //                for (int col = 0; col < slots.GetLength(1); col++)
-                //                {
-                //                    slots[row, col] = rngspoke.Next(10);
-                //                    Console.Write(slots[row, col]);
-                //                }
-                //                Console.WriteLine();
-                //            };
+                }
 
-                //        }
-                //display coin count
-                //ask how many coins to play
-                //run slot
-                //if statement on slot results
-                //loop 
+                //if (slots[1,0] == slots[1,1] && slots[1,0] == slots[1,2])
+                //{
+                //    //win
+                //}
+
 
 
             }
