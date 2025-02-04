adce0016fc8259a5c4eadba7fafa5806d960ba88
JayMayse <maysembcc@gmail.com>
2025-02-04 00:36:15 -0600

    cleaning up copies.  added the c, l, d

--- Program.cs
+++ Program.cs
@@ -12,12 +12,14 @@ Console.WriteLine("*** How to Win ***");
 if (GameCoins > 0)
 {
     Console.WriteLine("Which way would you like to play?");
-    Console.WriteLine("Press:\nC for Center\nL for Lines\nD for Diaganol");
+    Console.WriteLine($"You currently have {GameCoins}");
+    Console.WriteLine("Press:\nC for Center\nL for Lines\nD for Diaganol\n");
     string choices = Console.ReadLine().ToLower();
+    Console.WriteLine("");
 
-    if (choices != "c" && choices != "d" && choices != "r")
+    if (choices != "c" && choices != "d" && choices != "l")
     {
-        Console.WriteLine("Please choose between C, R, or D.");
+        Console.WriteLine("Please choose between C, L, or D.");
     }
     else
     {
@@ -33,88 +35,54 @@ if (GameCoins > 0)
             {
                 slots[row, col] = rngspoke.Next(10);
                 Console.Write(slots[row, col]);
-
-                Random rngspoke = new Random();
-                int clicks1 = rngspoke.Next(0, 10);
-                int clicks2 = rngspoke.Next(0, 10);
-                int clicks3 = rngspoke.Next(0, 10);
-                int[,] slots = new int[3, 3];
-
-                for (int row = 0; row < slots.GetLength(0); row++)
-                {
-                    for (int col = 0; col < slots.GetLength(1); col++)
-                    {
-                        slots[row, col] = rngspoke.Next(10);
-                        Console.Write(slots[row, col]);
-
-                    }
-
-                    Console.WriteLine();
-                }
-
-                bool isWinner = true;
-
-                for (int col = 1; col < slots.GetLength(1); col++)
-                {
-
-                    if (slots[1, 0] != slots[1, col])
-                    {
-                        isWinner = false;
-                        Console.WriteLine("You have FAILED!");
-                    }
-
-
-
-                }
             }
-
             Console.WriteLine();
         }
 
+
         bool isWinner = false;
         if (choices == "c")
-        for (int col = 1; col < slots.GetLength(1); col++)
-        {
-
-            if (slots[1, 0] == slots[1, col])
+            for (int col = 1; col < slots.GetLength(1); col++)
             {
-                isWinner = true;
-                Console.WriteLine("Congratulations! You won the center line");
-                GameCoins += 1;
-                break;
-            }
+
+                if (slots[1, 0] == slots[1, col])
+                {
+                    isWinner = true;
+                    Console.WriteLine("Congratulations! You won the center line");
+                    GameCoins += 1;
+                    break;
+                }
                 if (!isWinner)
                 {
                     Console.WriteLine("Sorry, you failed.");
                 }
-                else if (choices == "l")
-                    for (int row = 0; row < slots.GetLength(0); row++)
-                    {
-                        for (int row = 0; col < slots.GetLength(1); row++)
-
-                            if (slots[row, 0] == slots[row, 1] && slots[row, 0] == slots[row, 2])
-                            {
-                                Console.WriteLine("Congratulations you won on the lines");
-                                isWinner = true;
-                                GameCoins += 3;
-                                break;
-                            }
-                        if (!isWinner)
-                        {
-                            Console.WriteLine("Sorry, you have failed");
-                        }
-                        else if (choices == "d")
-                            if (slots[0, 0] == slots[1, 1] && (slots[0, 0]) == slots[2, 2])
-                            {
-                                Console.WriteLine("Congratulations you won on the diaganol");
-                                isWinner = true;
-                                GameCoins += 5;
-                                break;
-                            }
-                    }
-
-
+            }
+        else if (choices == "l")
+            for (int row = 0; row < slots.GetLength(0); row++)
+            {
+                if (slots[row, 0] == slots[row, 1] && slots[row, 0] == slots[row, 2])
+                {
+                    Console.WriteLine("Congratulations you won on the lines");
+                    isWinner = true;
+                    GameCoins += 3;
+                    break;
+                }
+                if (!isWinner)
+                {
+                    Console.WriteLine("SOrry, you have failed");
+                }
+            }
+        else if (choices == "d")
+            if (slots[0, 0] == slots[1, 1] && (slots[0, 0]) == slots[2, 2])
+            {
+                Console.WriteLine("Congratulations you won on the diaganol");
+                isWinner = true;
+                GameCoins += 5;
+                //break;
+            }
+        if (!isWinner)
+        {
+            Console.WriteLine("SOrry, you have FAiled");
         }
-
     }
 }
